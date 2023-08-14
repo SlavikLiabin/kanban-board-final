7e55f241618320bd158365af7998a55e8b88dc1e
BatanAndrei <yabatan777@gmail.com>
2023-08-14 14:02:28 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Layout } from './Components/Layout/layout';
 import { Header } from './Components/Header/header';
 import { Footer } from './Components/Footer/footer';
 import { Board } from './Components/Board/board';
+import { CardDetail } from './Components/Board/CardDetail/cardDetail';
 
 
 function App() {
@@ -12,7 +13,8 @@ function App() {
             <Layout>
                 <Header />
                     <main>
-                        <Board />
+                        <CardDetail />
+                        {/* <Board /> */}
                     </main>
                 <Footer />
             </Layout>
--- src/Components/Board/CardDetail/cardDetail.tsx
+++ src/Components/Board/CardDetail/cardDetail.tsx
@@ -1,8 +1,23 @@
+import style from './cardDetail.module.css'
+import { SvgCross } from '../../Shared/Svg/svgCross';
+import { ButtonСross } from '../../Shared/Buttons/buttonСross';
+
+
 export const CardDetail = () => {
 
     return (
-        <div>
-            cardDetail
+        <div className={style.containerDetail}>
+            <div className={style.windowDetail}>
+                <div className={style.wrapperTextArea}>
+                    <textarea className={style.name}></textarea>
+                    <textarea className={style.description}></textarea>
+                </div>
+                <div className={style.cross}>
+                    <ButtonСross>
+                        <SvgCross />
+                    </ButtonСross>
+                </div>
+            </div>
         </div>
     )
 }
