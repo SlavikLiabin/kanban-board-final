b88ed88729d75a216500901279b7d21a7b679cdc
BatanAndrei <yabatan777@gmail.com>
2023-08-17 18:44:20 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,29 @@ import { Footer } from './Components/Footer/footer';
 import { Board } from './Components/Board/board';
 import { CardDetail } from './Components/Board/CardDetail/cardDetail';
 import { ContextWrapper } from './Components/Context/taskContext';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 
 function App() {
+
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <Board />
+        },
+        {
+            path: "/tasks/:cardId",
+            element: <CardDetail />
+        }
+        ])
+
     return (
         <div className={'app'}>
             <ContextWrapper>
                 <Layout>
                     <Header />
                         <main>
-                            {/* <CardDetail /> */}
-                            <Board />
+                            <RouterProvider router={router}/>
                         </main>
                     <Footer />
                 </Layout>
--- src/Components/Board/CardDetail/cardDetail.tsx
+++ src/Components/Board/CardDetail/cardDetail.tsx
@@ -1,10 +1,12 @@
 import style from './cardDetail.module.css'
 import { SvgCross } from '../../Shared/Svg/svgCross';
 import { ButtonCross } from '../../Shared/Buttons/buttonCross';
+import { useNavigate } from 'react-router-dom';
 
 
 export const CardDetail = () => {
-
+    const navigate = useNavigate();
+    const navigateBack = () => navigate(-1);
     return (
         <div className={style.containerDetail}>
             <div className={style.windowDetail}>
@@ -13,10 +15,10 @@ export const CardDetail = () => {
                     <textarea className={style.description}></textarea>
                     <button className={style.buttonSave}>Save Card</button>
                 </div>
-                <div className={style.cross}>
-                    <ButtonCross>
+                <div>
+                    <button className={style.cross} onClick={navigateBack}>
                         <SvgCross />
-                    </ButtonCross>
+                    </button>
                 </div>
             </div>
         </div>
